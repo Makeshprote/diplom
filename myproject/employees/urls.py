b88e7048a1f70b86830287f4121a394f64c3ba72
Makeshprote <proofzziwnl@gmail.com>
2024-01-26 12:13:26 +0600

    updated gitignore

--- myproject/employees/urls.py
+++ myproject/employees/urls.py
@@ -5,7 +5,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('event_list/', views.event_list),
     path('', admin.site.urls),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
